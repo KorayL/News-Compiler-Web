bdc1aa16e5c7128d1ee1c5e21a293fd57e8749ed
klatif <klatif@ncsu.edu>
2025-06-11 22:04:04 -0400

    [Implementation] Bash scripts to obtain certificates and deploy website.

---
+++ deploy_global.sh
@@ -0,0 +1,33 @@
+# Create a local certificate so certbot can use it
+chmod +x get_certificate.sh
+./get_certificate.sh > /dev/null 2>&1
+
+# Check if the certificate generation was successful
+if [[ $? -ne 0 ]]; then
+  echo "Local Certificate generation failed..."
+  echo "Exiting."
+  exit 1
+else
+  echo "Local Certificate generated successfully..."
+fi
+
+# Start Docker Compose
+docker-compose up -d --build
+
+# Check if Docker Compose started successfully
+if [[ $? -ne 0 ]]; then
+  echo "Docker Compose failed to start..."
+  echo "Exiting."
+  exit 1
+else
+  echo "Docker Compose started successfully..."
+fi
+
+# Create the public certifications
+docker exec react_nginx certbot --nginx -d ${DOMAIN} -d www.${DOMAIN} --non-interactive --agree-tos --email ${EMAIL}
+
+# Update nginx configuration to use the new certificates
+sed -i "/selfsigned/ s/    /    # /" /etc/nginx/conf.d/default.conf  # Comment out self-signed certificate lines
+sed -i "/letsencrypt/ s/# //" /etc/nginx/conf.d/default.conf  # Uncomment Let's Encrypt certificate lines
+
+docker-compose restart frontend
--- get_certificate.sh
+++ get_certificate.sh
@@ -2,4 +2,17 @@ mkdir -p frontend/certificates
 cd frontend/certificates || exit
 
 # Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt
+grep -v "#" ../../cert_info.txt | \
+  openssl req -x509 -noenc -days 365 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt
+
+echo ""
+
+if [[ $? -eq 0 ]]; then
+  echo "Local Certificate generation failed..."
+  echo "Exiting."
+  exit 1
+fi
+
+echo ""  # New line for better readability
+
+cd ../../  # Return to the previous directory
