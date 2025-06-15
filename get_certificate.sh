ddd692069ae961fe3f8f6804b4f7840eeea7cb40
Koray Latif <51007317+KorayL@users.noreply.github.com>
2025-06-15 01:55:56 -0400

    Certificate and DB Storage (#12)
    Certbot certificates and MySQL data are now stored on the host via mounted directories.

--- get_certificate.sh
+++ get_certificate.sh
@@ -1,5 +1,5 @@
-mkdir -p frontend/certificates
-cd frontend/certificates || exit
+mkdir -p frontend/certificates/openssl
+cd frontend/certificates/openssl || exit
 
 # Generate a self-signed certificate
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt
