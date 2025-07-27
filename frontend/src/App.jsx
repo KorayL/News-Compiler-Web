f38306cc7cbd06c1f17730920acdeac7f554858c
klatif <klatif@ncsu.edu>
2025-07-26 21:15:50 -0400

    [Implementation] Lifted the API call to get articles.
    Also added a fun loading indicator icon as the app loads.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,19 @@ import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 
 import MosaicComponent from './components/MosaicComponent.jsx'
 import ArticleComponent from "./components/ArticleComponent.jsx";
+import { MosaicDataProvider } from './components/MosaicData.jsx';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/home" />} />
-                <Route path="/home" element={<MosaicComponent/>} />
-                <Route path="/article/:articleId" element={<ArticleComponent/>} />
-                <Route path="*" element={<h1>Not Found</h1>} />
-            </Routes>
+            <MosaicDataProvider>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    <Route path="/home" element={<MosaicComponent/>} />
+                    <Route path="/article/:articleId" element={<ArticleComponent/>} />
+                    <Route path="*" element={<h1>Not Found</h1>} />
+                </Routes>
+            </MosaicDataProvider>
         </BrowserRouter>
     )
 }
