22056150527c7ee40c0cc8582b59ba7a4ca21543
klatif <klatif@ncsu.edu>
2025-07-27 00:01:04 -0400

    [Refactor] Separating files by page and created `ArticleTile` component.

---
+++ frontend/src/pages/mosaic/componets/ArticleTile.jsx
@@ -0,0 +1,45 @@
+import {useNavigate} from "react-router-dom";
+import PropTypes from "prop-types";
+
+/**
+ * ArticleTile component displays a single article tile with its title, image, and source.
+ * @param id - The ID of the article.
+ * @param title - The title of the article.
+ * @param imageUrl - The URL of the article's image (optional).
+ * @param source - The source from which the article was scraped.
+ * @returns {JSX.Element} The rendered article tile component.
+ */
+const ArticleTile = ({id, title, imageUrl, source}) => {
+    ArticleTile.propTypes = {
+        id: PropTypes.oneOfType([PropTypes.number]).isRequired,
+        title: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string,
+        source: PropTypes.string.isRequired,
+    };
+
+    /** Navigation hook to navigate to different pages */
+    const navigate = useNavigate()
+
+    /**
+     * Opens an article by its ID.
+     * @param articleId - The ID of the article to open.
+     */
+    function openArticle(articleId) {
+        console.log("Opening article with ID: " + articleId)
+        navigate(`/article/${articleId}`)
+    }
+
+    // Construct an article div
+    return (<div className="article" key={id}
+                 onClick={() => openArticle(id)}>
+
+        <h4 className="tileTitle" id={title}>{title}</h4>
+        {/*Only include the image if it exists*/}
+        {imageUrl && <img src={imageUrl} alt={title}></img>}
+
+        {/*Show where the article was scraped from*/}
+        <p> {source} </p>
+    </div>)
+}
+
+export default ArticleTile
