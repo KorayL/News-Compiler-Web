1efb6ae5e24250b27a2bf7108aca6782934698e1
klatif <klatif@ncsu.edu>
2025-08-02 18:35:16 -0400

    [Refactor] Move category types to sub-packages.

--- scraper/src/sites/__init__.py
+++ scraper/src/sites/__init__.py
@@ -1,8 +1,28 @@
-import os
+import pkgutil
+import importlib
+import inspect
+
+from src.Site import Site
 
 # contains all modules to be imported when "from sites import *" is called
 __all__ = []
-for file_name in os.listdir(os.path.realpath(__file__).removesuffix("__init__.py")):
-    if "__" not in file_name:
-        file_name = file_name[:-3]
-        __all__.append(file_name)
+
+for finder, modname, ispkg in pkgutil.walk_packages(__path__, prefix=__name__ + "."):
+
+    # Modules that start with an underscore are considered private and not imported
+    if modname.split(".")[-1].startswith("_"):
+        continue
+
+    try:
+        # Attempt to import the module
+        module = importlib.import_module(modname)
+    except ImportError as e:
+        # If the module cannot be imported, print an error message and continue
+        print(f"Error importing module {modname}: {e}")
+        continue
+
+    # Check if the module has classes that are subclasses of Site
+    for name, obj in inspect.getmembers(module, inspect.isclass):
+        if issubclass(obj, Site) and obj is not Site and obj.__module__ == modname:
+            globals()[name] = obj
+            __all__.append(name)
